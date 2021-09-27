7c25374f44a362c56932c6d954193466e25dc13d
dlannan <dlannan@internode.on.net>
2021-09-27 21:49:34 +0930

    Working defold test.

---
+++ example/swampy-example.lua
@@ -0,0 +1,406 @@
+local tinsert 	= table.insert
+local tremove 	= table.remove
+
+local swampy 	= require "swampy.swampy"
+local url 		= require "swampy.utils.url"
+local json 		= require "swampy.utils.json"
+
+-- ---------------------------------------------------------------------------
+-- Game level states.
+--   These track where you are in the login and running process
+local GAME		 	= {
+	LOGGING_IN 		= 1,
+	LOGIN_OK		= 2,
+	LOGIN_FAIL		= 3,
+	
+	SETUP			= 10,
+
+	GAME_JOINING	= 20, 
+
+	EXIT 			= 90,
+}
+
+-- ---------------------------------------------------------------------------
+-- User defined events - these are handled in your module
+local USER_EVENT 	= {
+	REQUEST_GAME 	= 1,
+	POLL 			= 2,
+
+	REQUEST_READY	= 10,
+	REQUEST_START 	= 20,
+	REQUEST_WAITING = 30,
+	REQUEST_ROUND 	= 40,
+}
+
+-- ---------------------------------------------------------------------------
+local function resetclient(self)
+	self.device_id = url.uuid()
+end 
+
+-- ---------------------------------------------------------------------------
+
+local function websocket_callback(self, conn, data)
+	if data.event == websocket.EVENT_DISCONNECTED then
+		log("Disconnected: " .. tostring(conn))
+		self.connection = nil
+		update_gui(self)
+	elseif data.event == websocket.EVENT_CONNECTED then
+		update_gui(self)
+		log("Connected: " .. tostring(conn))
+	elseif data.event == websocket.EVENT_ERROR then
+		log("Error: '" .. data.message .. "'")
+	elseif data.event == websocket.EVENT_MESSAGE then
+		log("Receiving: '" .. tostring(data.message) .. "'")
+	end
+end
+
+-- ---------------------------------------------------------------------------
+-- Setup server 
+local function setup_swampy(self, modulename)
+
+	swampy.setmodulename(modulename) 
+
+	-- The Nakama server configuration
+	local config = {}
+	
+	config.host 		= "45.125.247.75"
+	config.port 		= 5000
+	config.use_ssl 		= true 
+	config.api_token 	= "j3mHKlgGZ4" 
+	
+	self.login_attemps 	= 0
+	self.gamestate 		= 0
+	
+
+	self.user_id		= "blahblah"
+	self.device_id		= 12345
+
+	self.swp_client = swampy.create_client(config)
+	--pprint(self.swp_client)
+end
+
+-- ---------------------------------------------------------------------------
+-- our login function using a device token
+local function device_login(self, callback)
+
+	if(self.debugClient == 1) then resetclient(self) end
+	--pprint("Config:")
+	--pprint(self.config_data)
+	
+	self.gamestate = GAME.LOGGING_IN
+	-- login using the token and create an account if the user
+	-- doesn't already exist
+	swampy.do_login(self.swp_client, self.user_id, self.device_id, function(resp, id, result)
+
+		local data = json.decode(result.response)
+		if data.status == "OK" then
+			-- store the token and use it when communicating with the server
+			self.swp_token = data.bearertoken
+			swampy.set_bearer_token(self.swp_client, self.swp_token)
+			--pprint(self.swp_client)
+			
+			print("Successful login")
+			if(callback) then callback() end
+			self.gamestate = GAME.LOGIN_OK
+			return
+		end
+	
+		print("Unable to login")
+		self.login_attemps = self.login_attemps + 1
+		if(self.login_attemps > 10) then 
+
+			self.gamestate = GAME.LOGIN_FAIL
+		else 
+			resetclient(self)
+			self.gamestate = GAME.SETUP
+		end
+	end)
+end
+
+
+-- ---------------------------------------------------------------------------
+-- Update match player name
+local function updateplayername( self, match_data )
+
+	for k,user in pairs(self.client_party.people) do 
+		if(user.user_id == match_data.presence.user_id) then 
+			local playerdata = json.decode(match_data.data)
+			user.player_name = playerdata.player_name
+		end
+	end
+end	
+
+-- ---------------------------------------------------------------------------
+local function connect(self, callback)
+
+	-- The connect process for the time beiing is to enable an account with user info 
+	--  attached to the device the user has and the user generated token they are connecting with.
+	-- Dont connect too many times
+	if(self.swp_client.state ~= "CONNECTING") then 
+		self.swp_client.state = "CONNECTING"
+		swampy.connect( self.swp_client, self.player_name, self.device_id, function(rdata)
+
+			if(rdata.status == "OK") then 
+				self.swp_account = rdata.data
+				-- Not sure this is needed anymore (nakama legacy)
+				self.client_id = self.swp_account.username 
+				self.user_id = self.swp_account.id
+
+				print("Connected ok.")
+			else 
+				print("Connect failed.")
+			end
+			self.swp_client.state = nil
+			callback(rdata.status)
+		end)
+	end
+end
+
+-- ---------------------------------------------------------------------------
+local function updateaccount(self, callback)
+
+	-- Only bother with display name initially
+	local useracct = self.swp_account
+	useracct.username = self.player_name
+	local body = swampy.update_user(self.swp_client, self.device_id, self.player_name, useracct.lang, function(resp)
+
+		-- pprint(resp)
+		if resp.status ~= "OK" then
+			print("Error:", resp.status)
+		end  
+		callback(resp)
+	end)
+end 
+
+-- ---------------------------------------------------------------------------
+local function make_requestgamestate(client, game_name, device_id) 
+
+	-- User submission data must be in this format - will be checked
+	local userdata = {
+
+		state       = GAME.GAME_JOINING,   
+		uid         = device_id,
+		name        = game_name,
+		round       = 0,
+		timestamp   = os.time(),
+
+		event       = USER_EVENT.REQUEST_GAME,
+		json        = "",
+	}
+	return userdata
+end 
+
+-- ---------------------------------------------------------------------------
+-- A normal updategame does a "request_round" event with no other data sent
+local function updategame(self, callback) 
+
+	if(self.game == nil) then return end 
+	
+	local body = make_requestgamestate( self.swp_client, self.game_name, self.device_id)
+	body.state = self.game.state or GAME.GAME_JOINING
+	local bodystr = json.encode(body)
+	swampy.game_update( self.swp_client, self.game_name, self.device_id, function(data)
+
+		if(data.status == "OK") then 
+			callback(data.result)
+		else 
+			print("Error updating game: ", data.results)
+			callback(nil)
+		end 
+	end, bodystr)
+end 
+
+-- ---------------------------------------------------------------------------
+-- An update that doesnt return anything, just keeps connect alive
+local function pollgame(self) 
+
+	local body = make_requestgamestate( self.swp_client, self.game_name, self.device_id )
+	body.event = USER_EVENT.POLL
+	local bodystr = json.encode(body)
+	swampy.game_update( self.swp_client, self.game_name, self.device_id, function(data)
+		-- Can do stuff here if you need something to happen ;)
+	end, bodystr)
+end 
+
+-- ---------------------------------------------------------------------------
+
+local function updateready(self, ready) 
+	
+	if(self.game.state ~= GAME.GAME_JOINING) then return end
+
+	local body = make_requestgamestate( self.swp_client, self.game_name, self.device_id )
+	body.event = USER_EVENT.REQUEST_READY
+	body.json = { state = tostring(ready or 0) }
+	local bodystr = json.encode(body)
+
+	swampy.game_update( self.swp_client, self.game_name, self.device_id, function(data)
+
+	end, bodystr)
+end 
+
+-- ---------------------------------------------------------------------------
+
+local function startgame(self, callback)
+
+	if(self.game.state ~= GAME.GAME_JOINING) then return end
+	
+	-- This sets the server game state to match the owner	
+	local body = make_requestgamestate( self.swp_client, self.game_name, self.device_id )
+	body.state = self.game.state
+	body.event = USER_EVENT.REQUEST_START
+	local bodystr = json.encode(body)
+	swampy.game_update( self.swp_client, self.game_name, self.device_id, function(data)
+
+		-- The game is returned on start - use this for game obj
+		if(data.status == "OK") then 
+			callback(data.result)
+		else 
+			print("Error starting game: ", data.results)
+		end 
+	end, bodystr)
+end 
+
+-- ---------------------------------------------------------------------------
+
+local function exitgame(self, callback)
+
+	local body = make_requestgamestate( self.swp_client, self.game_name, self.device_id )
+	body.state = GAME.EXIT
+	local bodystr = json.encode(body)
+	swampy.game_update( self.swp_client, self.game_name, self.device_id, function(data)
+		callback()
+	end, bodystr)
+end 
+
+-- ---------------------------------------------------------------------------
+
+local function updategamestate(self, callback)
+
+	if(self.game.state < GAME.GAME_JOINING) then return end
+
+	local body = make_requestgamestate( self.swp_client, self.game_name, self.device_id )
+	body.state = self.game.state
+	body.event = USER_EVENT.REQUEST_ROUND
+	local bodystr = json.encode(body)
+	swampy.game_update( self.swp_client, self.game_name, self.device_id, function(data)
+
+		-- The game is returned on start - use this for game obj
+		if(data.status == "OK") then 
+			callback(data.result)
+		else 
+			print("Error updating round: ", data.results)
+		end 
+	end, bodystr)
+end 
+
+-- ---------------------------------------------------------------------------
+
+local function waiting(self)
+
+	if(self.game == nil) then pprint("Invalid Game: ", self.game); return end 
+	
+	local body = make_requestgamestate( self.swp_client, self.game_name, self.device_id )
+	body.state = self.game.state or GAME.GAME_JOINING
+	body.event = USER_EVENT.REQUEST_WAITING
+	local bodystr = json.encode(body)
+	pprint("WAITING: "..tostring(body.state))
+	swampy.game_update( self.swp_client, self.game_name, self.device_id, function(data)
+
+		-- The game is returned on start - use this for game obj
+		if(data.status ~= "OK") then 
+			print("Error updating scenario: ", data.results)
+		end 
+	end, bodystr)
+end 
+
+-- ---------------------------------------------------------------------------
+
+local function doupdate(self, callback)
+	
+	updategame(self, function(data) 
+
+		-- Replace incoming data for the game object 
+		if(data) then 
+			self.game = data
+
+			if(self.game) then 
+				updategamestate(self, function(data)
+				self.round = tmerge(self.round, data)
+				if(callback) then callback(data) end
+				if(self.game == nil or self.game.state == nil) then return end
+				if(self.gamestate ~= self.game.state) then 
+				end
+			end)
+			end
+			
+		-- Something has kicked us out return to previous page
+		else
+			self.swp_account = nil 
+			self.game = nil
+		end
+	end)
+end 
+
+-- ---------------------------------------------------------------------------
+-- Just a wrapper in case want to insert extra functionality
+local function findgame( self, gamename, callback )
+
+	swampy.game_find( self.swp_client, gamename, self.device_id, callback, nil )
+end
+
+-- ---------------------------------------------------------------------------
+-- Just a wrapper in case want to insert extra functionality
+local function creategame( self, gamename, callback )
+
+	local limit = 10 -- 10 players - this is not being used in MyGame.
+	swampy.game_create( self.swp_client, gamename, self.device_id, limit, callback )
+end
+
+-- ---------------------------------------------------------------------------
+
+local function joingame( self, gamename, callback )
+
+	swampy.game_join( self.swp_client, gamename, self.device_id, callback, nil )
+end
+
+-- ---------------------------------------------------------------------------
+
+local function leavegame( self, gamename, callback )
+
+	swampy.game_leave( self.swp_client, gamename, self.device_id, callback, nil )
+end
+
+-- ---------------------------------------------------------------------------
+
+local function closegame( self, gamename, callback )
+
+	swampy.game_close( self.swp_client, gamename, self.device_id, callback, nil )
+end
+
+-- ---------------------------------------------------------------------------
+return {
+	setup 			= setup_swampy,
+	login 			= device_login,
+	connect 		= connect,
+	updateaccount 	= updateaccount,
+	resetclient		= resetclient,
+
+	creategame 		= creategame,
+	findgame		= findgame,
+	joingame		= joingame,
+	leavegame		= leavegame,
+	closegame		= closegame,
+	
+	updategame		= updategame,
+	pollgame		= pollgame,
+	doupdate		= doupdate,
+	updateready		= updateready,
+	startgame		= startgame,
+	exitgame		= exitgame,
+
+	updateround 	= updateround,
+
+	waiting 		= waiting,
+}
+
+-- ---------------------------------------------------------------------------
---
+++ swampy/swampy.lua
@@ -0,0 +1,305 @@
+
+local json = require "swampy.utils.json"
+local url  = require "swampy.utils.url"
+
+-- ---------------------------------------------------------------------------
+
+local swampy = {
+	GAME_MODULENAME		= "MyGame",				-- Default dummy game module
+}
+
+local end_points = {
+	user 	= {
+		login 			= "/user/login", 		--%?userid={name}&uid={device_uid}",
+		authenticate 	= "/user/authenticate", --%?logintoken={token}&uid={device_uid}",
+		connect 		= "/user/connect", 		--%?logintoken={token}&uid={device_uid}",
+		update 			= "/user/update", 		--%?playername={playername}&uid={device_uid}&lang={lang_tag}&username={username}
+	},
+	data 	= {
+		gettable 		= "/data/gettable", 	--%?name={dbname}&limit={row_limit}",
+		settable 		= "/data/settable", 	--%?name={dbname}",  + body of data
+	},
+	game 	= {
+		create 			= "/game/create",		-- ?name={game_name}&uid={device_id}&limit={max_players}
+		find 			= "/game/find",			-- ?name={game_name}&uid={device_id}
+		join 			= "/game/join",			-- ?name={game_name}&uid={device_id}
+		leave 			= "/game/leave",		-- ?name={game_name}&uid={device_id}
+		update 			= "/game/update",		-- ?name={game_name}&uid={device_id}
+		close 			= "/game/close",		-- ?name={game_name}&uid={device_id}
+	},
+}
+
+-- ---------------------------------------------------------------------------
+
+local function genname()
+	m,c = math.random,("").char 
+	name = ((" "):rep(9):gsub(".",function()return c(("aeiouy"):byte(m(1,6)))end):gsub(".-",function()return c(m(97,122))end))
+	return(string.sub(name, 1, math.random(4) + 5))
+end
+
+-- ---------------------------------------------------------------------------
+
+local function setmodulename( name )
+	swampy.GAME_MODULENAME = name
+end
+
+-- ---------------------------------------------------------------------------
+-- TODO: Make a nice way to do blocking requests
+local function http_request(url, method, header)
+	local co = coroutine.running()
+	print("fetching...", url, method, header)
+	http.request(url, method, function(self, id, response)
+		print('result is fetched')
+		coroutine.resume(co, response)
+	end, header)
+	return coroutine.yield()
+end
+
+local function http_req_blocking( url, method, header )
+
+	local co = coroutine.create(function()
+		print('request is executed')
+		local response = http_request(url, method, header)
+		print('should not be executed before fetched result')
+	end)
+
+	local ok, err = coroutine.resume(co)
+	print(ok, err)
+	co2 = coroutine.running()
+	pprint(co2)
+-- 	while(ok == true) do
+-- 		co2 = coroutine.running()
+-- 		print(ok, err)
+-- 	end
+end 
+
+-- ---------------------------------------------------------------------------
+-- TODO: If a relatime port is needed we will use this 
+--
+-- function init(self)
+-- 	self.url = "ws://echo.websocket.org"
+-- 	local params = {
+-- 		timeout = 3000,
+-- 		headers = "Sec-WebSocket-Protocol: chat\r\nOrigin: mydomain.com\r\n"
+-- 	}
+-- 	self.connection = websocket.connect(self.url, params, websocket_callback)
+-- end
+-- 
+-- -- ---------------------------------------------------------------------------
+-- 
+-- function finalize(self)
+-- 	if self.connection ~= nil then
+-- 		websocket.disconnect(self.connection)
+-- 	end
+-- end
+-- 
+
+-- ---------------------------------------------------------------------------
+
+local function http_result(self, _, response)
+	print(response.status)
+	print(response.response)
+	pprint(response.headers)
+end
+
+-- ---------------------------------------------------------------------------
+
+local function create_client(cfg) 
+
+	local client = cfg 
+	client.method = cfg.method or "GET"
+	client.scheme = "http://"
+	if(cfg.use_ssl) then client.scheme = "https://" end
+	client.uri = client.scheme..client.host..":"..client.port
+	client.base_path = "/api/v1/"..cfg.api_token
+	client.handler = http_result
+	return client 
+end 
+
+
+-- ---------------------------------------------------------------------------
+
+local function do_login( client, userid, device_id, callback )
+
+	local fail = nil
+	if(userid == nil) then fail = true end
+	if(device_id == nil) then fail = true end
+
+	if(fail == true) then  
+		callback(nil, nil, { response = json.encode( { status = 'ERR' } ) } )
+		return
+	end
+
+	local qlogin = url.parse(client.uri..client.base_path..end_points.user.login)
+	qlogin.query.userid = userid
+	qlogin.query.uid = device_id
+
+	-- store in client for caching 
+	client.uid = userid 
+	client.device_id = device_id 
+	
+	-- We do a quick handshake to create a bearer token.
+	http.request(tostring(qlogin), client.method, function(self, _, response)
+
+		if(response.response and string.len(response.response) > 0) then 
+			local resp = json.decode(response.response)
+			local qauth = url.parse(client.uri..client.base_path..end_points.user.authenticate)
+			qauth.query.logintoken = resp.uuid
+			qauth.query.uid = device_id 
+		
+			http.request(tostring(qauth), client.method, callback)
+		end
+	end)
+end
+
+-- ---------------------------------------------------------------------------
+
+local function connect(client, name, device_id, callback) 
+
+	local qtable = url.parse(client.uri..client.base_path..end_points.user.connect)
+	qtable.query.module = swampy.GAME_MODULENAME
+	qtable.query.name = name or genname()
+	qtable.query.uid = device_id
+
+	local header = { ["Authorization"] = client.bearertoken or "" }
+
+	http.request(tostring(qtable), client.method, function(self, _, resp)
+		
+		if(resp.response) then 
+			local respdata = json.decode(resp.response)
+			callback(respdata)
+		end
+	end, header)
+end
+
+-- ---------------------------------------------------------------------------
+
+local function get_table( client, name, limit, callback )
+
+	local qtable = url.parse(client.uri..client.base_path..end_points.data.gettable)
+	qtable.query.name = name
+	qtable.query.limit = limit
+
+	http.request(tostring(qtable), client.method, function(self, _, resp)
+
+		if(resp.response) then 
+			callback(resp)
+		end
+	end)
+end
+
+-- ---------------------------------------------------------------------------
+
+local function set_table( client, name, data, callback )
+
+	local qtable = url.parse(client.uri..client.base_path..end_points.data.settable)
+	qtable.query.name = name
+	headers = {}
+	if(type(data) ~= "string") then return nil end
+	body = data		-- must be string
+	
+	http.request(tostring(qtable), "POST", function(self, _, resp)
+
+		if(resp.response) then 
+			callback(resp)
+		end
+	end, headers, body)
+end
+
+-- ---------------------------------------------------------------------------
+
+local function set_bearer_token( client, btoken )
+
+	client.bearertoken = btoken 
+end
+
+-- ---------------------------------------------------------------------------
+
+local function update_user( client, device_id, playername, lang_tag, callback )
+
+	local quser = url.parse(client.uri..client.base_path..end_points.user.update)
+	quser.query.playername = playername 
+	quser.query.lang = lang_tag
+	quser.query.uid = device_id
+
+	local header = { ["Authorization"] = client.bearertoken or "" }
+	
+	http.request(tostring(quser), client.method, function(self, _, resp)
+
+		if(resp.response) then 
+			local respdata = json.decode(resp.response)
+			callback(respdata)
+		end
+	end, header)
+end 
+
+-- ---------------------------------------------------------------------------
+
+local function game_create( client, gamename, device_id, limit, callback )
+
+	local qgame = url.parse(client.uri..client.base_path..end_points.game.create)
+	qgame.query.name = gamename
+	qgame.query.uid = device_id
+	qgame.query.limit = limit
+
+	local header = { ["Authorization"] = client.bearertoken or "" }
+
+	http.request(tostring(qgame), client.method, function(self, _, resp)
+
+		if(resp.response) then 
+			local respdata = json.decode(resp.response)
+			callback(respdata)
+		end
+	end, header)
+end 
+
+-- ---------------------------------------------------------------------------
+
+local function game_func( client, ep, gamename, device_id, callback, body )
+
+	local qgame = url.parse(client.uri..client.base_path..ep)
+	qgame.query.name = gamename 
+	qgame.query.uid = device_id
+	-- qgame.query.tick = os.time()
+
+	local header = { ["Authorization"] = client.bearertoken or "" }
+	local method = client.method 
+	if(body) then method = "POST" end
+
+	-- Special blocking requests - should rarely be used!!!
+	-- if(callback == nil) then 
+	-- 	print("Blocking request")
+	-- 	http_req_blocking(tostring(qgame), method, header)
+	-- else
+		http.request(tostring(qgame), method, function(self, _, resp)
+
+			if(resp.response) then 
+				local respdata = json.decode(resp.response)
+				callback(respdata)
+			end
+		end, header, body)
+	-- end 
+end 
+
+-- ---------------------------------------------------------------------------
+
+return {
+
+	setmodulename		= setmodulename,		-- Set the module name that is to be used
+	create_client 		= create_client,		-- create client http connect details
+	do_login 			= do_login,				-- handshake and create a bearertoken which is device + uid
+	connect 			= connect,				-- create a username <-> device + uid
+	set_bearer_token	= set_bearer_token,		-- set client bearertoken from server login
+	update_user 		= update_user,			-- set user details (name, lang etc)
+
+	get_table 			= get_table,			-- get a table from the server
+	set_table 			= set_table,			-- set a table on the server
+
+	game_create 		= game_create,
+	game_find 			= function(c, gn, uid, cb, body) game_func(c, end_points.game.find, gn, uid, cb, body) end,
+	game_join 			= function(c, gn, uid, cb, body) game_func(c, end_points.game.join, gn, uid, cb, body) end,
+	game_leave 			= function(c, gn, uid, cb, body) game_func(c, end_points.game.leave, gn, uid, cb, body) end,
+	game_update 		= function(c, gn, uid, cb, body) game_func(c, end_points.game.update, gn, uid, cb, body) end,
+	game_close			= function(c, gn, uid, cb, body) game_func(c, end_points.game.close, gn, uid, cb, body) end,
+}
+-- ---------------------------------------------------------------------------
